10d010eb8ede4ac74168007d56c8f8921a986392
Le Goff Alban <alban.le-goff@etudiant.univ-rennes1.fr>
2021-02-19 10:04:25 +0100

    f

--- view.js
+++ view.js
@@ -6,20 +6,23 @@ Rectangle.prototype.paint = function(ctx) {
     ctx.stroke();
   };
   
-  Line.prototype.paint = function(ctx) {
+Line.prototype.paint = function(ctx) {
     //TODO Manager color
     ctx.beginPath();
     ctx.moveTo(this.getInitX(), this.getInitY());
     ctx.lineTo(this.getFinalX(), this.getFinalY());
     ctx.stroke();
-  };
+};
   
-  Drawing.prototype.paint = function(ctx) {
+Drawing.prototype.paint = function(ctx) {
     //console.log(this.getForms());
-    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color FF9933
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     this.getForms().forEach(function (eltDuTableau) {
       // now fill the canvas
       eltDuTableau.paint(ctx);
-    });
-  
+});
+
+forme.prototype.paint = function(ctx) {
+    
+}
