f948d6dc78100b601bf1ead2a543d7645ef6d072
Arthur Lalande-Marchand <arthur.lalande-marchand@etudiant.univ-rennes1.fr>
2021-02-19 08:50:57 +0100

    Test + ajout des attributs dans la classe interaction

--- interaction.js
+++ interaction.js
@@ -3,9 +3,20 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
-
+	Element.setAttribute(DebutX, 0);
+	Element.setAttribute(DebutY, 0);
+	Element.setAttribute(FinX, 0);
+	Element.setAttribute(FinY, 0);
 	// Developper les 3 fonctions gérant les événements
-
+	function selectionner(evt){
+		const draggableElement = document.getElementById(evt);
+	}
+	function deplacer(evt){
+		
+	}
+	function lacher(evt){
+		
+	}
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
 
