26e17fa929d40ef22602d11e23d1f7ce21165d74
Le Goff Alban <alban.le-goff@etudiant.univ-rennes1.fr>
2021-02-19 09:50:19 +0100

    push

--- model.js
+++ model.js
@@ -1,3 +1,15 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function drawing() {
+    this.formes=new Array();
+}
+drawing.prototype.addForm=function(f){
+    this.formes.push(f);
+}
+
+function forme(t, c) {
+    this.taille=t;
+    this.color=c;
+}
